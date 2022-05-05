876bb164da7b54a3865dbf6cdc1c4ffe51e6cc09
yeogh <52830435+yeogh@users.noreply.github.com>
2022-05-05 23:35:33 +0800

    added sql for create tables; removed unused callApi.js

--- backend/database.sql
+++ backend/database.sql
@@ -4,18 +4,33 @@ CREATE TABLE users(
     user_id SERIAL PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
     password VARCHAR(128) NOT NULL,
-    email VARCHAR(50) NOT NULL
-);
+    email VARCHAR(50) NOT NULL,
+    permission VARCHAR(5) NOT NULL,
 
-INSERT INTO users (name, password, email) VALUES ('CD Eng', 'example', 'cde@gmail.com');
+);
 
 CREATE TABLE products(
     product_id SERIAL PRIMARY KEY,
-    code VARCHAR(10),
-    name VARCHAR(30),
-    size VARCHAR(3),
+    code VARCHAR(20) NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    size VARCHAR(3) NOT NULL,
     option VARCHAR(20),
-    quantity INT4,
-    quantity_optimal INT4,
-    percent_optimal NUMERIC(5,2),
+    quantity INT NOT NULL,
+    quantity_optimal INT NOT NULL,
+    created_by SERIAL NOT NULL,
+    quantity_sold INT DEFAULT 0,
+
+    FOREIGN KEY (created_by)
+        REFERENCES users (user_id)
 )
+
+CREATE TABLE sales(
+    sales_id SERIAL PRIMARY KEY,
+    quantity_sold INT NOT NULL,
+    date TIMESTAMP (255) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    product_id SERIAL NOT NULL,
+    
+    FOREIGN KEY (product_id)
+        REFERENCES products (product_id)
+);
+
